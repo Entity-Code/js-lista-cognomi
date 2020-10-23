22930aef57f932c8cf08cbe9572173c9d6f95cd7
Mattia <mattia.tummolilllo@gmail.com>
2020-10-23 16:23:16 +0200

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,39 +1,53 @@
-// ARRAY LISTA COGNOMI
-var listaCognome = ["barretta","tummolillo","cante","campanile","barbato","rossi","ferrari","russo","bianchi"];
 
-// CHIEDIAMO ALL'UTENTE INPUT SUO COGNOME
-var inputCognome = prompt("Inserisci il tuo cognome")
+var invia = document.getElementById("invia");
+invia.addEventListener("click",
+	function() {
+      // ARRAY LISTA COGNOMI
+     var listaCognome = ["barretta","tummolillo","cante","campanile","barbato","rossi","ferrari","russo","bianchi"];
 
-//AGGIUNGO CON PUSH IL COGNOME INSERITO DALL'UTENTE
-listaCognome.push(inputCognome);
+     // INPUT COGNOME UTENTE
+     var inputCognome = document.getElementById("inputCognome");
 
-// ORDINO ALFABETICAMENTE LA LISTA
-listaCognome.sort(confrontaS);
+     //AGGIUNGO CON PUSH IL COGNOME INSERITO DALL'UTENTE
+     listaCognome.push(inputCognome.value);
 
-//FIX ORDINAMENTO LETTERE MAIUSCOLE MISTE A LETTERE MINUSCOLE
-function confrontaS(a,b) {
-  minA = a.toLowerCase();
-  minB = b.toLowerCase();
-  if (minA < minB) { return -1; }
-      else {
-         if (minA > minB) { return 1; }
-        else { return 0; }
-            }
-  }
+     // ORDINO ALFABETICAMENTE LA LISTA
+     listaCognome.sort(confrontaS);
 
-// OUTPUT
-   // STAMPO LA LISTA NELL'UL TRAMITE DEGLI LI
-   var listaCognomeli = "";
-   var i = 0;
-   while (i < listaCognome.length){
-      var appoggio = listaCognome[i];
-      listaCognomeli = listaCognomeli + "<li>" + appoggio + "</li>";
-      i++;
-   }
-   document.getElementById("lista").innerHTML = listaCognomeli;
+     //FIX ORDINAMENTO LETTERE MAIUSCOLE MISTE A LETTERE MINUSCOLE
+     function confrontaS(a,b) {
+       minA = a.toLowerCase();
+       minB = b.toLowerCase();
+       if (minA < minB) { return -1; }
+           else {
+              if (minA > minB) { return 1; }
+             else { return 0; }
+                 }
+       }
+
+     // OUTPUT
+        // STAMPO LA LISTA NELL'UL TRAMITE DEGLI LI
+        var listaCognomeli = "";
+        var i = 0;
+        while (i < listaCognome.length){
+           var appoggio = listaCognome[i];
+           listaCognomeli = listaCognomeli + "<li>" + appoggio + "</li>";
+           i++;
+        }
+
+
+        document.getElementById("lista").innerHTML = listaCognomeli;
 
 
 
-   // STAMPO LA POSIZIONE DEL COGNOME INSERITO NELLA LISTA "UMANAMENTE"
-   var posizione = listaCognome.indexOf(inputCognome);
-   document.getElementById("posizione").innerHTML =  "Ti trovi nella posizione numero: " + (++posizione);
+        // STAMPO LA POSIZIONE DEL COGNOME INSERITO NELLA LISTA "UMANAMENTE"
+        var posizione = listaCognome.indexOf(inputCognome.value);
+        document.getElementById("posizione").innerHTML =  "Ti trovi nella posizione numero: " + (++posizione);
+
+
+      // DISPLAY ON/OFF
+      // document.getElementById("risultati").reset();
+      var risultati = document.getElementById("risultati");
+      risultati.className = "dBlock";
+	}
+);
